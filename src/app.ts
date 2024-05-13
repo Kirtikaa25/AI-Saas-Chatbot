a5eb9f09dc7d2c48a6a8448e179baabf8420ce33
Kirtika Gautam <100124517+Kirtikaa25@users.noreply.github.com>
2024-05-13 21:14:11 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,10 +10,6 @@ const app=express();
 app.use(cors({origin:"http://localhost:5173",credentials:true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-
-
-
 app.use(morgan("dev"));
-
 app.use("/api/v1",appRouter);
 export default app;
